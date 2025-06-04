32e76a26c62aa18db0c8b033270d78ca53811762
AbishakeSrithar <sritharabishake@gmail.com>
2025-06-04 09:58:48 +0100

    Home Page + Gaming Page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,102 +2,59 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+    <div className="overflow-scroll h-screen snap-y snap-mandatory">
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+
+      <div className="snap-center grid grid-rows-[2fr_5fr_2fr] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+        <h1>Hello World!</h1>
+        <div className="grid grid-rows-[2fr_5fr] items-center justify-items-center" id="main">
+          <p className="text-center">My name is Abishake!<br></br>I'm a programmer/gamer B)</p>
+          <p className="text-center">Welcome to my little corner of the internet</p>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        <div id="footer">
+          <a href="https://github.com/AbishakeSrithar" className="text-green-500 hover:text-green-500 active:text-blue-500 underline m-5" target="_blank">Github</a>
+          <a href="https://www.youtube.com/@Lilshakee" className="text-red-500 hover:text-red-500 active:text-blue-500 underline m-5" target="_blank">YouTube</a>
+          <p className="text-center mt-3 text-blue-500 underline">Discord:</p>
+          <p className="text-center">Lil_shakee</p>
+        </div>
+      </div>
+
+      
+      <div className="snap-center grid grid-rows-[2fr_6fr_2fr] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 text-xs">
+        <div>
+          <h1 className="text-4xl">Gaming!</h1>
+        </div>
+        <div className="" id="main">
+          <p className="m-5">I'm an avid gamer and have been since I was 5yrs old</p>
+          <br></br>
+          <div className="m-5">
+            <p>Games I'm currently playing:</p>
+            <br></br>
+            <ul>
+              <li>- Valorant</li>
+              <li>- Dead Cells</li>
+            </ul>
+            <br></br>
+          </div>
+          
+          <div className="m-5">
+            <p>Games I've enjoyed in the past:</p>
+            <br></br>
+            <ul>
+              <li>- Call of Duty (MW2/3, BO1/2/3)</li>
+              <li>- Fortnite</li>
+              <li>- Hollow Knight</li>
+              <li>- Hades 1/2</li>
+              <li>- Portal 1/2</li>
+            </ul>
+          </div>
+        </div>
+        <div id="footer" className="items-center justify-items-center">
+          <h1 className="text-center text-xs">Checkout my Gaming Channel: </h1>
+          <br></br>
+          <a href="https://www.youtube.com/@Lilshakee" className="text-red-500 hover:text-red-500 active:text-blue-500 underline" target="_blank">YouTube</a>
+        </div>
+      </div>
     </div>
   );
 }
