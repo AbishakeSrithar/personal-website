ef0110bdf13813ee4a06067396c833de0fa4f85e
AbishakeSrithar <sritharabishake@gmail.com>
2025-06-09 13:36:08 +0100

    Add pictures, commented out my pics for now

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,61 +1,13 @@
 "use client";
 import CodingCarousel from "@/components/codingCarousel";
-import PacmanGame from "@/components/game";
 import GamingCarousel from "@/components/gamingCarousel";
-import Image from "next/image";
-import TypewriterComp from "@/components/typewriter";
+import HomePage from "@/components/homePage";
 export default function Home() {
   return (
     <main>
       {/* ------------------------ Home ----------------------- */}
       <div className="overflow-scroll h-screen snap-y snap-mandatory">
-        <PacmanGame />
-        <div className="snap-center flex flex-col items-center justify-center min-h-screen p-8 pb-20 gap-8 sm:p-20">
-          <div className="">
-            <h1 className="text-2xl">Hello World!</h1>
-          </div>
-          <div className="items-center justify-items-center" id="main">
-            <h3>
-              <TypewriterComp />
-            </h3>
-            <p className="text-center m-5">
-              Welcome to my little corner of the internet!
-            </p>
-          </div>
-          <div
-            id="footer"
-            className="grid grid-cols-2 grid-rows-2 gap-4 w-[60vw] md:w-[15vw]"
-          >
-            <Image
-              src="/images/me_1.png"
-              alt="Profile Pic 1"
-              width={250}
-              height={250}
-              className=""
-            />
-            <Image
-              src="/images/me_2.png"
-              alt="Profile Pic 1"
-              width={250}
-              height={250}
-              className=""
-            />
-            <Image
-              src="/images/me_3.png"
-              alt="Profile Pic 1"
-              width={250}
-              height={250}
-              className=""
-            />
-            <Image
-              src="/images/me_4.png"
-              alt="Profile Pic 1"
-              width={250}
-              height={250}
-              className=""
-            />
-          </div>
-        </div>
+        <HomePage/>
 
         {/* ------------------------ Games ----------------------- */}
         <div className="snap-center flex flex-col items-center justify-center min-h-screen p-8 pb-20 gap-8 sm:p-20 text-xs">
---
+++ src/components/homePage.tsx
@@ -0,0 +1,143 @@
+"use client";
+import Image from "next/image";
+import PacmanGame from "@/components/game";
+import TypewriterComp from "@/components/typewriter";
+
+export default function HomePage() {
+  return (
+    <div className="">
+      <PacmanGame />
+      <div className="snap-center flex flex-col items-center justify-center min-h-screen p-8 pb-20 gap-8 sm:p-20">
+        <div className="">
+          <h1 className="text-2xl">Hello World!</h1>
+        </div>
+        <div className="items-center justify-items-center" id="main">
+          <h3>
+            <TypewriterComp />
+          </h3>
+          <p className="text-center m-5">
+            Welcome to my little corner of the internet!
+          </p>
+        </div>
+        <div
+          id="footer"
+          className="grid grid-cols-4 grid-rows-3 gap-4 m w-[60vw] md:w-[30vw]"
+        >
+          <Image
+            src="/images/insta1.png"
+            alt="Profile Pic 5"
+            width={250}
+            height={250}
+            className=""
+          />
+          <Image
+            src="/images/insta2.png"
+            alt="Profile Pic 5"
+            width={250}
+            height={250}
+            className=""
+          />
+          <Image
+            src="/images/insta3.png"
+            alt="Profile Pic 5"
+            width={250}
+            height={250}
+            className=""
+          />
+          <Image
+            src="/images/insta4.png"
+            alt="Profile Pic 5"
+            width={250}
+            height={250}
+            className=""
+          />
+          
+          <Image
+            src="/images/food.png"
+            alt="Profile Pic 5"
+            width={250}
+            height={250}
+            className=""
+          />
+          <Image
+            src="/images/rgbpc.png"
+            alt="Profile Pic 5"
+            width={250}
+            height={250}
+            className=""
+          />
+          <Image
+            src="/images/setup.png"
+            alt="Profile Pic 5"
+            width={250}
+            height={250}
+            className=""
+          />
+          <Image
+            src="/images/posters.png"
+            alt="Profile Pic 5"
+            width={250}
+            height={250}
+            className=""
+          />
+          {/* <Image
+            src="/images/me_2.png"
+            alt="Profile Pic 5"
+            width={250}
+            height={250}
+            className=""
+          />
+          <Image
+            src="/images/me_3.png"
+            alt="Profile Pic 6"
+            width={250}
+            height={250}
+            className=""
+          />
+          <Image
+            src="/images/me_4.png"
+            alt="Profile Pic 7"
+            width={250}
+            height={250}
+            className=""
+          />
+          <Image
+            src="/images/me_4.png"
+            alt="Profile Pic 8"
+            width={250}
+            height={250}
+            className=""
+          /> */}
+          <Image
+            src="/images/insta5.png"
+            alt="Profile Pic 5"
+            width={250}
+            height={250}
+            className=""
+          />
+          <Image
+            src="/images/insta6.png"
+            alt="Profile Pic 6"
+            width={250}
+            height={250}
+            className=""
+          />
+          <Image
+            src="/images/insta7.png"
+            alt="Profile Pic 7"
+            width={250}
+            height={250}
+            className=""
+          />
+          <Image
+            src="/images/insta8.png"
+            alt="Profile Pic 8"
+            width={250}
+            height={250}
+            className=""
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
